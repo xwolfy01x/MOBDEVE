267aa1cc0d991e951fdca07f120ce6d4c82c1c0a
Jeremy Chi <jeremy_chi@dlsu.edu.ph>
2021-05-18 16:10:27 +0800

    Game View

--- app/src/main/java/com/mobdeve/cactus/mobdevemp/LoginActivity.java
+++ app/src/main/java/com/mobdeve/cactus/mobdevemp/LoginActivity.java
@@ -2,13 +2,60 @@ package com.mobdeve.cactus.mobdevemp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mobdeve.cactus.mobdevemp.dao.UserDAOSQLImpl;
+import com.mobdeve.cactus.mobdevemp.models.User;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ArrayList<User> userList;
+    private Button btn_register3, btn_login3;
+    private EditText et_userName2, et_pass2;
+    UserDAOSQLImpl databaseAccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        databaseAccess = new UserDAOSQLImpl(getApplicationContext());
+        init();
+    }
+
+    public void init() {
+        btn_register3 = (Button) findViewById(R.id.btn_register3);
+        btn_login3 = (Button) findViewById(R.id.btn_login3);
+
+        et_userName2 = (EditText) findViewById(R.id.et_userName2);
+        et_pass2 = (EditText) findViewById(R.id.et_pass2);
+
+        btn_register3.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        btn_login3.setOnClickListener(v -> {
+            userList = databaseAccess.getUsers();
+            for(int i = 0; i < userList.size(); i++){
+                String user = et_userName2.getText().toString();
+                String pass = et_pass2.getText().toString();
+
+                if(user.equalsIgnoreCase(userList.get(i).getUsername()) && pass.equalsIgnoreCase(userList.get(i).getPassword())) {
+                    Intent intent = new Intent(getApplicationContext(), RunningManAnimation.class);
+                    startActivity(intent);
+                }
+                //next activity
+	            else Toast.makeText(getApplicationContext(), "Invalid Username or Password!", Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
--- app/src/main/java/com/mobdeve/cactus/mobdevemp/RegisterActivity.java
+++ app/src/main/java/com/mobdeve/cactus/mobdevemp/RegisterActivity.java
@@ -2,13 +2,45 @@ package com.mobdeve.cactus.mobdevemp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.mobdeve.cactus.mobdevemp.dao.UserDAOSQLImpl;
+import com.mobdeve.cactus.mobdevemp.models.User;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button btn_register2, btn_login2;
+    private EditText et_name1, et_userName1, et_pass1;
+    UserDAOSQLImpl databaseAccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        databaseAccess = new UserDAOSQLImpl(getApplicationContext());
+        init();
+    }
+
+    public void init() {
+        btn_register2 = (Button) findViewById(R.id.btn_register2);
+        btn_login2 = (Button) findViewById(R.id.btn_login2);
+        et_name1 = (EditText) findViewById(R.id.et_name1);
+        et_userName1 = (EditText) findViewById(R.id.et_userName1);
+        et_pass1 = (EditText) findViewById(R.id.et_pass1);
+
+        btn_login2.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        btn_register2.setOnClickListener(view -> {
+            User newUser = new User(et_name1.getText().toString(), et_userName1.getText().toString(), et_pass1.getText().toString());
+            databaseAccess.addUser(newUser);
+        });
     }
 }
